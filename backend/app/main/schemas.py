7e030c48795d11901fa1347852020c34f24f843b
zzzrg <guo.zrui@gmail.com>
2020-05-07 01:54:33 -0400

    Migrated Food entity to SQLalchemy

--- backend/app/main/schemas.py
+++ backend/app/main/schemas.py
@@ -1,4 +1,8 @@
+from datetime import date
+
 from pydantic import BaseModel
+
+from .models import FoodEnum
 
 
 class VendorBase(BaseModel):
@@ -12,6 +16,27 @@ class VendorCreate(VendorBase):
 
 
 class Vendor(VendorBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+
+class FoodBase(BaseModel):
+    name: str
+    weight: float
+    date_produced: date
+    expiry_date: date = None
+    description: str = ""
+    category: FoodEnum
+    serving_size: str = ""
+
+
+class FoodCreate(FoodBase):
+    pass
+
+
+class Food(FoodBase):
     id: int
 
     class Config:
